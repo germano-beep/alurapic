68b14a7fcf257b49039749773ac88ad0c1000bfe
germano-beep <garthur83@gmail.com>
2022-09-28 11:30:54 -0300

    data binding

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'ap-photo',
   templateUrl: 'photo.component.html'
 })
 export class PhotoComponent  {
-  url = 'https://images.unsplash.com/photo-1663530294185-5af3692326c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60';
-  description = 'mountain'
+  @Input() url = '';
+  @Input() description = ''
 }
